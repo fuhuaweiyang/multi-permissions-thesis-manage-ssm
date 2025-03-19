26eb2925f9cfd847f704043ba3ba65bdb175391f
fuhuaweiyang <shuiyue2530@gmail.com>
2025-03-19 10:55:07 +0800

    first commit

---
+++ src/main/java/com/rabbiter/ol/config/MyWebMvnConfig.java
@@ -0,0 +1,16 @@
+package com.rabbiter.ol.config;
+
+import com.rabbiter.ol.tool.PathUtils;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// 进行文件映射
+@Configuration
+public class MyWebMvnConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/file/videoFile/**").addResourceLocations("file:" + PathUtils.getClassLoadRootPath() + "/file/videoFile/");
+        registry.addResourceHandler("/file/imageFile/**").addResourceLocations("file:" + PathUtils.getClassLoadRootPath() + "/file/imageFile/");
+    }
+}
